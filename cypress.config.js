cd969f0e205b40ff87dd413a95677e4696464c56
almohamed <alaa.mohamed@cegedim.com>
2023-12-05 02:09:26 +0200

    publish project installtions

---
+++ cypress.config.js
@@ -0,0 +1,34 @@
+
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+
+  'cypress-cucumber-preprocessor': {
+
+    nonGlobalStepDefinitions: false,
+
+    stepDefinitions: 'cypress/integrations/*/*',
+
+  },
+  e2e: {
+
+    setupNodeEvents(on, config) {
+ 
+      return require('./cypress/plugins/index.js')(on, config)
+
+    },
+    baseUrl: 'https://opensource-demo.orangehrmlive.com/',
+
+    specPattern: 'cypress/integration/**/*.feature',
+
+    supportFile:false
+
+  },
+  env: {
+    TAGS: '@testing',
+    // ProBaseURL: 'https://pro.staging.maiia.com',
+    
+  },
+
+})
+
